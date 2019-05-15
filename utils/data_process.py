b62bb96e6bcd993c5b0c791a589dca44ce5b0735
DannyWu1996 <danny199607@gmail.com>
2019-05-15 14:24:20 +0800

    "fix data processing problem"

--- utils/data_process.py
+++ utils/data_process.py
@@ -69,21 +69,41 @@ def save_df(file_path, df_object):
 		column to call the mapping
 '''
 
+def single_df_set(df, col_name):
+	if col_name == 'item':
+		city_list = np.unique(np.concatenate(
+			(df['dpt'].unique(), df['dst'].unique()),
+			0
+		)).tolist()
+		return city_list
+	else:
+		key_set = np.unique(df[col_name]).tolist()
+		return key_set
 
-def build_map(df, col_name):
+def build_map(df, dropped_df, col_name):
 	# print df.columns.values
 	if col_name == 'item':
 		city_list = np.unique(np.concatenate(
-			(df['dpt'].unique(), df['dst'].unique()), 0)).tolist()
+			(df['dpt'].unique(),
+			 df['dst'].unique(),
+			 dropped_df['dpt'].unique(),
+			 dropped_df['dst'].unique()
+			 ), 0)).tolist()
 		# print city_list
 		key = sorted(city_list)
 		m = dict(zip(key, range(len(key))))
 		df['dpt'] = df['dpt'].map(lambda x: m[x])
+		dropped_df['dpt'] = dropped_df['dpt'].map(lambda x: m[x])
 		df['dst'] = df['dst'].map(lambda x: m[x])
+		dropped_df['dst'] = dropped_df['dst'].map(lambda x: m[x])
 	else:
-		key = sorted(df[col_name].unique().tolist())
+		key = sorted(np.unique(np.concatenate(
+			(df[col_name].unique(),
+			dropped_df[col_name].unique()
+			), 0)).tolist())
 		m = dict(zip(key, range(len(key))))
 		df[col_name] = df[col_name].map(lambda x: m[x])
+		dropped_df[col_name] = dropped_df[col_name].map(lambda x: m[x])
 	return m, key
 
 
@@ -145,8 +165,15 @@ def remove_user_record(history_df, min_order):
 		index += len(hist)
 
 	print("\tremove user nums: %d" % remove_user_nums)
+	dropped_df = history_df.loc[history_df.index[remove_records]]
 	history_df = history_df.drop(history_df.index[remove_records])
-	return history_df
+
+	u = np.unique(history_df['user_id'])
+	dropped_u = np.unique(dropped_df['user_id'])
+	if len(np.intersect1d(u, dropped_u))>0:
+		raise BaseException("user intersection error!!!!")
+
+	return history_df, dropped_df
 
 
 '''
@@ -176,10 +203,10 @@ def travel_net_gen(travel_net_df, item_map):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--preprocess", type=bool,
-					help="choose whether to process the data", default=None)
-parser.add_argument("-s", "--do_split", type=str,
-					help="Split raw json file to specific format. Format, uid, dpt, dst, order_id", default=None)
+parser.add_argument("-c", "--preprocess", action='store_true',
+					help="choose whether to process the data")
+parser.add_argument("-s", "--do_split", action='store_true',
+					help="Split raw json file to specific format. Format, uid, dpt, dst, order_id")
 parser.add_argument("-r", "--raw_history_data_path", type=str,
 					help="Path to raw history data. Json format, with column user_id, dpt, dst, time, order_id")
 parser.add_argument("-m", "--min_order", type=int,
@@ -188,8 +215,9 @@ parser.add_argument("-a", "--aux_path", type=str,
 					help="Path to processed data, including user2id, city2id mappings")
 parser.add_argument("-u", "--raw_user_data_path", type=str,
 					help="Path to raw user data, Json format, with columns user_id, age, gender, travel_miles", default='')
-parser.add_argument("-l", "--long_seq", type=bool, help='cutting strategy for long or short sequence', default=None)
+parser.add_argument("-l", "--long_seq", action="store_false", help='cutting strategy for long or short sequence')
 parser.add_argument("-sr", "--split_ratio", type=float, help="user_based split ratio for test and training dataset", default=0.8)
+parser.add_argument("--nonhybrid", action="store_true", help='using hybrid dataset trainset prefix')
 args = parser.parse_args()
 
 do_split = args.do_split
@@ -200,7 +228,7 @@ aux_path = args.aux_path
 path_user = args.raw_user_data_path
 long_seq = args.long_seq
 split_ratio = args.split_ratio
-
+nonhybrid = args.nonhybrid
 '''
 	history-wised splitting strategy
 '''
@@ -219,11 +247,12 @@ if do_preprocess:
 		  ('True' if do_split is not None else 'False'))
 	print("\tminimum order number - %d" % min_order)
 	print("\tcutting long sequence - %s" % ("True" if long_seq else "False"))
+	print("\tnonhybrid - %s" % ("True" if nonhybrid else "False"))
 	print("==============================================================================")
 
 	with open('../raw_data/itny/city_air_map.pkl', 'rb') as f:
 		city_list, air_list, air_city_map = pickle.load(f, encoding='bytes')
-	if do_split is not None:
+	if do_split:
 		# print do_split
 		do_split_data(do_split, path_history, city_list)
 
@@ -231,27 +260,32 @@ if do_preprocess:
 		os.mkdir(aux_path)
 
 	history_df = json_to_df(path_history)
-	history_df = remove_user_record(history_df, min_order)
+	history_df, dropped_df = remove_user_record(history_df, min_order)
 
 	#### build the mapping for each dataframe column
-	user_map, user_key = build_map(history_df, 'user_id')
-	item_map, item_key = build_map(history_df, 'item')
-	order_map, order_key = build_map(history_df, 'order_id')
+	user_map, user_key = build_map(history_df, dropped_df, 'user_id')
+	item_map, item_key = build_map(history_df, dropped_df, 'item')
+	order_map, order_key = build_map(history_df, dropped_df, 'order_id')
 
-	#     cate_list = [i for i in range(item_count)]
-#     cate_list = np.array(cate_list, dtype=np.int32)
 
-	#generate random category for each item
 	cate_list = np.arange(len(item_map))
 
+	hist_user = single_df_set(history_df, 'user_id')
+	hist_item = single_df_set(history_df, 'item')
+	hist_order = single_df_set(history_df, 'order_id')
+
 	user_count, item_count, order_count, cate_count =\
-		len(user_map), len(item_map), len(order_map), len(cate_list)
+		len(hist_user), len(item_map), len(hist_order), len(cate_list)
 	print('user_count: %d\titem_count: %d\torder_count: %d\tcate_count: %d'
 		  % (user_count, item_count, order_count, cate_count))
 
 	history_df = history_df.sort_values(['user_id', 'time'])
 	history_df = history_df.reset_index(drop=True)
 	history_df = history_df[['user_id', 'order_id', 'dpt', 'dst', 'time']]
+
+	dropped_df = dropped_df.sort_values(['user_id', 'time'])
+	dropped_df = dropped_df.reset_index(drop=True)
+	dropped_df = dropped_df[['user_id', 'order_id', 'dpt', 'dst', 'time']]
 	# cate_list = [history_df[''][i] for i in range(len(history_df.index))]
 
 	#saving preprocessed data into file.
@@ -259,6 +293,7 @@ if do_preprocess:
 		pickle.dump(history_df, f)
 		pickle.dump(cate_list, f)
 		pickle.dump((user_count, item_count, order_count, cate_count), f)
+		pickle.dump(dropped_df, f)
 		print('create user history file: {}'.format(f.name))
 
 	with open(os.path.join(aux_path, 'mapping.pkl'), 'wb') as f:
@@ -284,6 +319,7 @@ else:
 	print("\tsaving preprocessed aux path - %s" % aux_path)
 	print("\tminimum order number - %d" % min_order)
 	print("\tcutting long sequence - %s" % ("True" if long_seq else "False"))
+	print("\tnonhybrid - %s" % ("True" if nonhybrid else "False"))
 	print("==============================================================================")
 
 with open(os.path.join(aux_path, 'history.pkl'), 'rb') as f:
@@ -291,6 +327,7 @@ with open(os.path.join(aux_path, 'history.pkl'), 'rb') as f:
 	cate_list = pickle.load(f, encoding='bytes')
 	user_count, item_count, order_count, cate_count = pickle.load(
 		f, encoding='bytes')
+	dropped_df = pickle.load(f, encoding='bytes')
 with open(os.path.join(aux_path, 'mapping.pkl'), 'rb') as f:
 	user_key, item_key, order_key = pickle.load(f, encoding='bytes')
 	user_map, item_map, order_map = pickle.load(f, encoding='bytes')
@@ -343,7 +380,7 @@ for user_order_id, history in history_df.groupby(['user_id', 'order_id']):
 	if user_order_id[0] != prev_user:
 		prev_user = user_order_id[0]
 		prev_hist = []
-	pos_list = merge_travel_route(history['dpt'].tolist(), history['dst'].tolist())
+	pos_list 	= merge_travel_route(history['dpt'].tolist(), history['dst'].tolist())
 	# user_all_hist[user_order_id[0]] = user_all_hist.get(user_order_id[0], []) + pos_list
 
 	prev_hist = prev_hist[:-1] if prev_hist and prev_hist[-1] == pos_list[0] else prev_hist
@@ -355,9 +392,11 @@ for user_order_id, history in history_df.groupby(['user_id', 'order_id']):
 	cur_hist = prev_hist + pos_list
 	city_tuple = zip(cur_hist[:-1], cur_hist[1:])
 	neg_hist_i = [gen_neg(cur, pos) for cur, pos in city_tuple]
-			
 
-	for i in range(1, len(pos_list)):
+	seq_begin = 1
+	if nonhybrid:
+		seq_begin = len(pos_list) - 2
+	for i in range(seq_begin, len(pos_list)):
 		hist = pos_list[:i]
 		if i != len(pos_list) - 1:
 			'''
@@ -368,8 +407,10 @@ for user_order_id, history in history_df.groupby(['user_id', 'order_id']):
 			else:
 				train_set.append((user_order_id[0], hist, pos_list[i], neg_hist_i[len(prev_hist)+1: len(prev_hist)+i]))
 		else:
-			user_current_hist[user_order_id[0]] = (user_order_id[0], prev_hist+hist, pos_list[i], neg_hist_i)
-			# user_current_hist[user_order_id[0]] = (user_order_id[0], hist, pos_list[i], neg_hist_i[len(prev_hist):])
+			if long_seq:
+				user_current_hist[user_order_id[0]] = (user_order_id[0], prev_hist+hist, pos_list[i], neg_hist_i)
+			else:
+			 	user_current_hist[user_order_id[0]] = (user_order_id[0], hist, pos_list[i], neg_hist_i[len(prev_hist):])
 	prev_hist += pos_list
 	user_hist[user_order_id[0]] = prev_hist
 test_set = [test for uid, test in user_current_hist.items()]
@@ -377,27 +418,61 @@ print('training set: %d, test set: %d' % (len(train_set), len(test_set)))
 # print '\tremove %d users with less than %d orders' % (len(remove_user, min_order))
 assert len(test_set) == user_count, "user number unmatch"
 assert len(user_hist.keys()) == user_count, "user number unmatch"
+
+'''
+	same process for dropped dataframe
+'''
+d_test_set = []
+d_user_current_hist = {}
+
+d_prev_user = 0
+d_prev_hist = []
+d_user_hist = {}
+
+for user_order_id, history in dropped_df.groupby(['user_id', 'order_id']):
+	if user_order_id[0] != d_prev_user:
+		d_prev_user = user_order_id[0]
+		d_prev_hist = []
+	pos_list 	= merge_travel_route(history['dpt'].tolist(), history['dst'].tolist())
+	d_prev_hist = d_prev_hist[:-1] if d_prev_hist and d_prev_hist[-1] == pos_list[0] else d_prev_hist
+
+	'''
+		prev hist city tuple, each tuple item contains previous city and current city,
+		use gen_neg to generate a list of negative sample w.r.t. corresponding index.
+	'''
+	cur_hist = d_prev_hist + pos_list
+	city_tuple = zip(cur_hist[:-1], cur_hist[1:])
+	neg_hist_i = [gen_neg(cur, pos) for cur, pos in city_tuple]
+	if long_seq:
+		d_user_current_hist[user_order_id[0]] = (user_order_id[0], d_prev_hist+pos_list[:-1], pos_list[-1], neg_hist_i)
+	else:
+		d_user_current_hist[user_order_id[0]] = (user_order_id[0], pos_list[:-1], pos_list[-1], neg_hist_i[len(prev_hist):])
+	d_prev_hist += pos_list
+	d_user_hist[user_order_id[0]] = d_prev_hist
+d_test_set = [test for uid, test in d_user_current_hist.items()]
+print('dropped test set %d' % (len(d_test_set)))
+
 with open(os.path.join(aux_path, 'dataset_{}.pkl'.format(('l' if long_seq else 's'))), 'wb') as f:
 	pickle.dump(train_set, f)
 	pickle.dump(test_set, f)
 	pickle.dump(travel_network, f)
 	pickle.dump(cate_list, f)
 	pickle.dump((user_count, item_count, order_count, cate_count), f)
+	pickle.dump(d_test_set, f)
 	print('create dataset file: {}'.format(f.name))
 
-with open(os.path.join(aux_path, 'user_hist.pkl'), 'wb') as f:
+with open(os.path.join(aux_path, 'user_hist_{}.pkl'.format(('l' if long_seq else 's'))), 'wb') as f:
 	pickle.dump(user_hist, f)
+	pickle.dump(d_user_hist, f)
 	print('create user history file: {}'.format(f.name))
 
 with open(os.path.join(aux_path, 'travel_network.pkl'), 'wb') as f:
 	pickle.dump(travel_network, f)
 	print('saving travel network file: {}'.format(f.name))
 
-
 '''
 	user-wised splitting strategy
 '''
-
 
 user_list = np.unique(history_df['user_id'].values)
 np.random.seed(1234)
@@ -407,7 +482,7 @@ rest_user = np.sort(user_list[int(user_count*split_ratio)::])
 print('train user: %d test user: %d'%(len(select_user), len(rest_user)))
 train_history_df = history_df[history_df['user_id'].isin(select_user)]
 test_history_df = history_df[history_df['user_id'].isin(rest_user)]
-
+dropped_df = dropped_df
 # print 'NAY' in item_map or 'PEK' in item_map
 train_set = []
 test_set = []
@@ -417,9 +492,6 @@ user_current_hist = {}
 prev_user = 0
 prev_hist = []
 user_hist = {}
-
-
-
 
 # generate train_set
 for user_order_id, history in train_history_df.groupby(['user_id', 'order_id']):
@@ -439,7 +511,10 @@ for user_order_id, history in train_history_df.groupby(['user_id', 'order_id']):
 	cur_hist = prev_hist + pos_list
 	city_tuple = zip(cur_hist[:-1], cur_hist[1:])
 	neg_hist_i = [gen_neg(cur, pos) for cur, pos in city_tuple]
-	for i in range(1, len(pos_list)):
+	seq_begin = 1
+	if nonhybrid:
+		seq_begin = len(pos_list) - 2
+	for i in range(seq_begin, len(pos_list)):
 		hist = pos_list[:i]
 		if i != len(pos_list) - 1:
 			if long_seq:
@@ -456,8 +531,7 @@ for user_order_id, history in test_history_df.groupby(['user_id', 'order_id']):
 		prev_hist = []
 	
 	pos_list = [history['dpt'].tolist()[0]] + history['dst'].tolist()
-	# user_all_hist[user_order_id[0]] = user_all_hist.get(user_order_id[0], []) + pos_list
-	
+
 	# avoid acycle
 	prev_hist = prev_hist[:-1] if prev_hist and prev_hist[-1] == pos_list[0] else prev_hist
 	cur_hist = prev_hist + pos_list
@@ -482,8 +556,11 @@ with open(os.path.join(aux_path, 'dataset_u{}.pkl'.format(("l" if long_seq else 
 	pickle.dump(travel_network, f)
 	pickle.dump(cate_list, f)
 	pickle.dump((user_count, item_count, order_count, cate_count), f)
+	pickle.dump(d_test_set, f)
 	print('create dataset file: {}'.format(f.name))
 
 with open(os.path.join(aux_path, 'user_hist_u{}.pkl'.format(('l' if long_seq else 's'))), 'wb') as f:
 	pickle.dump(user_hist, f)
+	pickle.dump(d_user_hist, f)
 	print('create user history file: {}'.format(f.name))
+
